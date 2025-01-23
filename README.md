148fc3574df053d95097313856fd5309bcfc1338
Edward Nguyen <ed@delimister.com>
2025-01-24 05:11:29 +0700

    :sparkles: well, magic happens here

--- README.md
+++ README.md
@@ -1,36 +1,10 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+## RevenueCat Telegram Notification Bot
+Well, I'm just too lazy to check RevenueCat dashboard or mobile app
+everytime so I decided to write this.
 
-## Getting Started
+Run it, push it to Vercel, free of charge.
+You can modify a little bit and format the response message yourself.
+As for now, I'm too lazy for this and decided to use GPT 3.5 turbo model
+to complete the task for me.
 
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+Use at your own risk :3
---
+++ src/app/webhook/route.ts
@@ -0,0 +1,43 @@
+import {NextRequest, NextResponse} from 'next/server';
+import axios from 'axios';
+import {OpenAI} from 'openai';
+
+// Telegram bot token and chat ID
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.CHAT_ID;
+const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+const OPENAI_API_KEY = process.env.OPENAI_KEY;
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json();
+    const signature = req.headers.get('Authorization')?.replace('Bearer ', '');
+    if (WEBHOOK_SECRET && signature !== WEBHOOK_SECRET) {
+      return NextResponse.json({message: 'Unauthorized'}, {status: 401});
+    }
+    const openai = new OpenAI({
+      apiKey: OPENAI_API_KEY,
+    });
+    const aiResponse = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo", messages: [{
+        role: 'system',
+        content: 'You are an assistant to write messages to user to notice about recent received RevenueCat purchases.\n' + 'Your main goal is format the data into a informative, interesting and concise message to send to user.\n' + 'You can use emojis to make your message more attractive. If price is not available, just write "Price not available".\nAnd do not say anything else.',
+      }, {
+        role: 'user',
+        content: `Here is the data:\n\n${JSON.stringify(body)}`,
+      }], max_tokens: 150,
+    });
+
+    const message = aiResponse.choices[0].message.content;
+    const telegramUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+    await axios.post(telegramUrl, {
+      chat_id: TELEGRAM_CHAT_ID,
+      text: message,
+    });
+
+    return NextResponse.json({message: 'Webhook received and processed successfully'});
+  } catch (error) {
+    console.log('err', error);
+    return NextResponse.json({message: 'Internal server error'}, {status: 500});
+  }
+}
